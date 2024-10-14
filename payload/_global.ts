5e0ec06b4ca5dd57a531383199ecc6d6c1bcc13d
EUNJI <107531537+eunjikwak@users.noreply.github.com>
2024-10-14 15:46:14 +0900

    seo 미리보기 추가

--- payload/_global.ts
+++ payload/_global.ts
@@ -1,9 +1,9 @@
 import favicon from '../asset/favicon.ico';
-import previewImage from '../asset/preview.jpg';
+import previewImage from '../asset/preview.png';
 import { IGlobal } from '../component/common/IGlobal';
 
-const title = 'RESUME-NEXTJS Sample Page';
-const description = 'This is a simple web resume sample.';
+const title = '1년차 개발자 곽은지 이력서';
+const description = '곽은지 이력서(포트폴리오)입니다.';
 
 export const _global: IGlobal.Payload = {
   favicon,
