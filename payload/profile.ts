d39b0f4ed7c0bb6468c65a12da83fedb67d44cd2
EUNJI <107531537+eunjikwak@users.noreply.github.com>
2024-10-21 19:22:26 +0900

    프로필이미지수정

--- payload/profile.ts
+++ payload/profile.ts
@@ -3,7 +3,7 @@ import { faBlogger, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 import { faBell } from '@fortawesome/free-regular-svg-icons';
 import { IProfile } from '../component/profile/IProfile';
-import image from '../asset/sample_tux.png';
+import image from '../asset/sample_tux.jpg';
 
 const profile: IProfile.Payload = {
   disable: false,
